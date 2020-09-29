f3311ae4f5d62f08242a7bdec0cc2b975ed3d6a1
Gus Martins <gustavosm994@gmail.com>
2020-09-28 21:34:36 -0300

    first commit - connected API

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,58 @@
+import React, { useState, FormEvent } from 'react';
+import Logo from '../../assets/logo.svg';
+import { Title, Form, Repository } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+};
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`)
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  };
+
+  return (
+    <>
+      <img src={Logo} alt="Github" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          onChange={(e) => setNewRepo(e.target.value)} />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repository>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img src={repository.owner.avatar_url}
+              alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repository>
+    </>
+  );
+};
+
+export default Dashboard;
