c6630c70f3a1f2fa32c64290c0b31e7119719b34
Gustavo Martins <atendimento9@cubosis.com.br>
2020-09-28 22:29:11 -0300

    capturing errors

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, FormEvent } from 'react';
 import Logo from '../../assets/logo.svg';
-import { Title, Form, Repository } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 import { FiChevronRight } from 'react-icons/fi';
 import api from '../../services/api';
 
@@ -15,14 +15,24 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
-    const response = await api.get<Repository>(`repos/${newRepo}`)
-    const repository = response.data;
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`)
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca por esse repositório');
+    }
   };
 
   return (
@@ -30,14 +40,16 @@ const Dashboard: React.FC = () => {
       <img src={Logo} alt="Github" />
       <Title>Explore repositórios no GitHub</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!! inputError} onSubmit={handleAddRepository}>
         <input
           placeholder="Digite o nome do repositório"
           onChange={(e) => setNewRepo(e.target.value)} />
         <button type="submit">Pesquisar</button>
       </Form>
 
-      <Repository>
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
         {repositories.map(repository => (
           <a key={repository.full_name} href="teste">
             <img src={repository.owner.avatar_url}
@@ -50,7 +62,7 @@ const Dashboard: React.FC = () => {
             <FiChevronRight size={20} />
           </a>
         ))}
-      </Repository>
+      </Repositories>
     </>
   );
 };
